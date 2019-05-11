849b8e2b66180b3a356f8b76b7adf0da8902c849
anastazjagaluza <38051431+anastazjagaluza@users.noreply.github.com>
2019-05-11 18:03:27 +0200

    finished
    finished

---
+++ javascript.js
@@ -0,0 +1,192 @@
+//January
+document.querySelector("#Jan").addEventListener("mouseover", () => {
+  document.querySelector("#Janline").style.display = "block";
+  document.querySelector("#Janline").classList.add("drawline");
+  document.querySelector("#Jandetails").style.display = "block";
+  document.querySelector("#Jansquare").classList.add("openup");
+  document.querySelector("#Jan").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Jan").addEventListener("mouseout", () => {
+  document.querySelector("#Janline").classList.remove("drawline");
+  document.querySelector("#Jansquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Jandetails").style.display = "none";
+  document.querySelector("#Janline").style.display = "none";
+});
+//February
+document.querySelector("#Feb").addEventListener("mouseover", () => {
+  document.querySelector("#Febline").style.display = "block";
+  document.querySelector("#Febline").classList.add("drawline");
+  document.querySelector("#Febdetails").style.display = "block";
+  document.querySelector("#Febsquare").classList.add("openup");
+  document.querySelector("#Feb").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Feb").addEventListener("mouseout", () => {
+  document.querySelector("#Febline").classList.remove("drawline");
+  document.querySelector("#Febsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Febdetails").style.display = "none";
+  document.querySelector("#Febline").style.display = "none";
+});
+//Mar
+document.querySelector("#March").addEventListener("mouseover", () => {
+  document.querySelector("#Marline").style.display = "block";
+  document.querySelector("#Marline").classList.add("drawline");
+  document.querySelector("#Mardetails").style.display = "block";
+  document.querySelector("#Marsquare").classList.add("openup");
+  document.querySelector("#March").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#March").addEventListener("mouseout", () => {
+  document.querySelector("#Marline").classList.remove("drawline");
+  document.querySelector("#Marsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Mardetails").style.display = "none";
+  document.querySelector("#Marline").style.display = "none";
+});
+//Apr
+document.querySelector("#Apr").addEventListener("mouseover", () => {
+  document.querySelector("#Aprline").style.display = "block";
+  document.querySelector("#Aprline").classList.add("drawline");
+  document.querySelector("#Aprdetails").style.display = "block";
+  document.querySelector("#Aprsquare").classList.add("openup");
+  document.querySelector("#Apr").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Apr").addEventListener("mouseout", () => {
+  document.querySelector("#Aprline").classList.remove("drawline");
+  document.querySelector("#Aprsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Aprdetails").style.display = "none";
+  document.querySelector("#Aprline").style.display = "none";
+});
+//May
+document.querySelector("#May").addEventListener("mouseover", () => {
+  document.querySelector("#Mayline").style.display = "block";
+  document.querySelector("#Mayline").classList.add("drawline");
+  document.querySelector("#Maydetails").style.display = "block";
+  document.querySelector("#Aprsquare").classList.add("openup");
+  document.querySelector("#May").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#May").addEventListener("mouseout", () => {
+  document.querySelector("#Mayline").classList.remove("drawline");
+  document.querySelector("#Maysquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Maydetails").style.display = "none";
+  document.querySelector("#Mayline").style.display = "none";
+});
+//Jun
+document.querySelector("#Jun").addEventListener("mouseover", () => {
+  document.querySelector("#Junline").style.display = "block";
+  document.querySelector("#Junline").classList.add("drawline");
+  document.querySelector("#Jundetails").style.display = "block";
+  document.querySelector("#Junsquare").classList.add("openup");
+  document.querySelector("#Jun").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Jun").addEventListener("mouseout", () => {
+  document.querySelector("#Junline").classList.remove("drawline");
+  document.querySelector("#Junsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Jundetails").style.display = "none";
+  document.querySelector("#Junline").style.display = "none";
+});
+//Jul
+document.querySelector("#Jul").addEventListener("mouseover", () => {
+  document.querySelector("#Julline").style.display = "block";
+  document.querySelector("#Julline").classList.add("drawline");
+  document.querySelector("#Juldetails").style.display = "block";
+  document.querySelector("#Julsquare").classList.add("openup");
+  document.querySelector("#Jul").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Jul").addEventListener("mouseout", () => {
+  document.querySelector("#Julline").classList.remove("drawline");
+  document.querySelector("#Julsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Juldetails").style.display = "none";
+  document.querySelector("#Julline").style.display = "none";
+});
+//Aug
+document.querySelector("#Aug").addEventListener("mouseover", () => {
+  document.querySelector("#Augline").style.display = "block";
+  document.querySelector("#Augline").classList.add("drawline");
+  document.querySelector("#Augdetails").style.display = "block";
+  document.querySelector("#Augsquare").classList.add("openup");
+  document.querySelector("#Aug").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Aug").addEventListener("mouseout", () => {
+  document.querySelector("#Augline").classList.remove("drawline");
+  document.querySelector("#Augsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Augdetails").style.display = "none";
+  document.querySelector("#Augline").style.display = "none";
+});
+//Sep
+document.querySelector("#Sep").addEventListener("mouseover", () => {
+  document.querySelector("#Sepline").style.display = "block";
+  document.querySelector("#Sepline").classList.add("drawline");
+  document.querySelector("#Sepdetails").style.display = "block";
+  document.querySelector("#Sepsquare").classList.add("openup");
+  document.querySelector("#Sep").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Sep").addEventListener("mouseout", () => {
+  document.querySelector("#Sepline").classList.remove("drawline");
+  document.querySelector("#Sepsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Sepdetails").style.display = "none";
+  document.querySelector("#Sepline").style.display = "none";
+});
+//Oct
+document.querySelector("#Oct").addEventListener("mouseover", () => {
+  document.querySelector("#Octline").style.display = "block";
+  document.querySelector("#Octline").classList.add("drawline");
+  document.querySelector("#Octdetails").style.display = "block";
+  document.querySelector("#Octsquare").classList.add("openup");
+  document.querySelector("#Oct").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Oct").addEventListener("mouseout", () => {
+  document.querySelector("#Octline").classList.remove("drawline");
+  document.querySelector("#Octsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Octdetails").style.display = "none";
+  document.querySelector("#Octline").style.display = "none";
+});
+//Nov
+document.querySelector("#Nov").addEventListener("mouseover", () => {
+  document.querySelector("#Novline").style.display = "block";
+  document.querySelector("#Novline").classList.add("drawline");
+  document.querySelector("#Novdetails").style.display = "block";
+  document.querySelector("#Novsquare").classList.add("openup");
+  document.querySelector("#Nov").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Nov").addEventListener("mouseout", () => {
+  document.querySelector("#Novline").classList.remove("drawline");
+  document.querySelector("#Novsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Novdetails").style.display = "none";
+  document.querySelector("#Novline").style.display = "none";
+});
+//Dec
+document.querySelector("#Dec").addEventListener("mouseover", () => {
+  document.querySelector("#Decline").style.display = "block";
+  document.querySelector("#Decline").classList.add("drawline");
+  document.querySelector("#Decdetails").style.display = "block";
+  document.querySelector("#Decsquare").classList.add("openup");
+  document.querySelector("#Dec").style.cursor = "pointer";
+  document.querySelector("#Line").style.strokeWidth = "10";
+});
+document.querySelector("#Dec").addEventListener("mouseout", () => {
+  document.querySelector("#Decline").classList.remove("drawline");
+  document.querySelector("#Decsquare").classList.remove("openup");
+  document.querySelector("#Line").style.strokeWidth = "1";
+  document.querySelector("#Decdetails").style.display = "none";
+  document.querySelector("#Decline").style.display = "none";
+});
